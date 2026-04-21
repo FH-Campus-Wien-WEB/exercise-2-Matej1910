e0d522e0baa60dad303280183f5fd181ed6cf401
Matej <matejderonja@hotmail.com>
2026-04-21 13:02:32 +0200

    Exercise 2 completed

--- server/files/index.js
+++ server/files/index.js
@@ -5,18 +5,83 @@ window.onload = function () {
     if (xhr.status == 200) {
       const movies = JSON.parse(xhr.responseText);
       for (const movie of movies) {
-        /* Task 1.3. Add your code from exercise 1 here 
-           and include a non-functional 'Edit' button
-           to pass this test */
-      }
+        const article = document.createElement('article')
+        article.id = movie.imdbID
+        const img = document.createElement('img')
+        img.src = movie.Poster
+        img.alt = movie.Title + ' Poster'
+        article.append(img)
+        const info = document.createElement('div')
+        info.className = 'movie-info'
+        const title = document.createElement('h1')
+        title.textContent = movie.Title
+        info.append(title)
 
+        // Edit button direkt nach dem Titel
+        const editButton = document.createElement('button')
+        editButton.textContent = 'Edit'
+        editButton.onclick = function () {
+          location.href = 'edit.html?imdbID=' + movie.imdbID
+        }
+        info.append(editButton)
+
+        const meta = document.createElement('p')
+        meta.className = 'meta'
+        const hours = Math.floor(movie.Runtime / 60)
+        const minutes = movie.Runtime % 60
+        meta.textContent = 'Runtime ' + hours + 'h ' + minutes + 'm • Released on ' + movie.Released
+        info.append(meta)
+        const genreDiv = document.createElement('div')
+        genreDiv.className = 'genres'
+        movie.Genres.forEach(function (genre) {
+          const genreSpan = document.createElement('span')
+          genreSpan.className = 'genre'
+          genreSpan.textContent = genre
+          genreDiv.append(genreSpan)
+        })
+        info.append(genreDiv)
+        const plot = document.createElement('p')
+        plot.textContent = movie.Plot
+        info.append(plot)
+        const dirTitle = document.createElement('h2')
+        dirTitle.textContent = movie.Directors.length === 1 ? 'Director' : 'Directors'
+        info.append(dirTitle)
+        const dirList = document.createElement('ul')
+        movie.Directors.forEach(function (director) {
+          const li = document.createElement('li')
+          li.textContent = director
+          dirList.append(li)
+        })
+        info.append(dirList)
+        const writTitle = document.createElement('h2')
+        writTitle.textContent = 'Writers'
+        info.append(writTitle)
+        const writList = document.createElement('ul')
+        movie.Writers.forEach(function (writer) {
+          const li = document.createElement('li')
+          li.textContent = writer
+          writList.append(li)
+        })
+        info.append(writList)
+        const actTitle = document.createElement('h2')
+        actTitle.textContent = 'Actors'
+        info.append(actTitle)
+        const actList = document.createElement('ul')
+        movie.Actors.forEach(function (actor) {
+          const li = document.createElement('li')
+          li.textContent = actor
+          actList.append(li)
+        })
+        info.append(actList)
+        const ratings = document.createElement('p')
+        ratings.className = 'ratings'
+        ratings.textContent = 'IMDb: ' + movie.imdbRating + '/10 • Metascore: ' + movie.Metascore
+        info.append(ratings)
+        article.append(info)
+        bodyElement.append(article)
+      }
     } else {
-      bodyElement.append(
-        "Daten konnten nicht geladen werden, Status " +
-          xhr.status +
-          " - " +
-          xhr.statusText
-      );
+      bodyElement.append("Daten konnten nicht geladen werden, Status " + xhr.status + " - " + xhr.statusText);
     }
   };
   xhr.open("GET", "/movies");
--- server/movie-model.js
+++ server/movie-model.js
@@ -1,2 +1,46 @@
-/* Task 1.1. Add your movie data here 
-   and export it so it's available in server.js */
+const movies = {
+  tt0816692: {
+    imdbID: 'tt0816692',
+    Title: 'Interstellar',
+    Released: '2014-11-07',
+    Runtime: 169,
+    Genres: ['Adventure', 'Drama', 'Sci-Fi'],
+    Directors: ['Christopher Nolan'],
+    Writers: ['Jonathan Nolan', 'Christopher Nolan'],
+    Actors: ['Matthew McConaughey', 'Anne Hathaway', 'Jessica Chastain'],
+    Plot: 'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
+    Poster: 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+    Metascore: 74,
+    imdbRating: 8.7
+  },
+  tt2179136: {
+    imdbID: 'tt2179136',
+    Title: 'American Sniper',
+    Released: '2015-01-16',
+    Runtime: 132,
+    Genres: ['Action', 'Biography', 'Drama'],
+    Directors: ['Clint Eastwood'],
+    Writers: ['Jason Hall', 'Chris Kyle', 'Scott McEwen'],
+    Actors: ['Bradley Cooper', 'Sienna Miller', 'Kyle Gallner'],
+    Plot: "Navy S.E.A.L. sniper Chris Kyle's pinpoint accuracy saves countless lives on the battlefield and turns him into a legend. Back home to his wife and kids after four tours of duty, however, Chris finds that it is the war he can't leave behind.",
+    Poster: 'https://m.media-amazon.com/images/M/MV5BMTkxNzI3ODQ4OV5BMl5BanBnXkFtZTgwMjkwMjY4MjE@._V1_SX300.jpg',
+    Metascore: 72,
+    imdbRating: 7.3
+  },
+  tt0111161: {
+    imdbID: 'tt0111161',
+    Title: 'The Shawshank Redemption',
+    Released: '1994-10-14',
+    Runtime: 142,
+    Genres: ['Drama'],
+    Directors: ['Frank Darabont'],
+    Writers: ['Stephen King', 'Frank Darabont'],
+    Actors: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+    Plot: 'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.',
+    Poster: 'https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_SX300.jpg',
+    Metascore: 82,
+    imdbRating: 9.3
+  }
+}
+
+module.exports = movies
